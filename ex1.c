1e56ebd8b7c77fe5e08f7955482e7140ba317c37
runcicta <runcicta@gmail.com>
2017-05-07 09:23:34 +0300

    Added exercises 1, 3 and Makefile

---
+++ ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* This is a comment*/
+
+//what is (int argc, char *argv[])
+int main (int argc, char *argv[])
+{
+   int distance = 100;
+
+   //This is also a comment
+
+   printf ("You are %d miles away.\n", distance);
+   printf ("I must lear C language before end of May\n");
+   printf ("I will dedicate my time to become greatest programmer\n");
+  
+   return 0;
+}
---
+++ ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 25;
+    int height = 187;
+
+    printf ("I am %d years old\n", age);
+    printf ("I am %d cm tall\n", height);
+
+}
